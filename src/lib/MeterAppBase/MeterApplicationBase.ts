1cdfc052d08dd360bee15d25efb5cac6684008bf
K sz2 <sugiura.k.nigo@gmail.com>
2019-09-16 01:31:00 +0900

    Mdify MeterApplicationBase to enable/disable preserveDrawingBuffer  by reading webstorage.

--- src/lib/MeterAppBase/MeterApplicationBase.ts
+++ src/lib/MeterAppBase/MeterApplicationBase.ts
@@ -224,13 +224,13 @@ export abstract class MeterApplicationBase extends PIXI.Application
      * @param height Stage height in px.
      * @param webSocketServerName Hostname of websocket server (default : same as webserver).s
      */
-    constructor(width: number, height: number, clearBeforeReneder? : boolean, transparent? : boolean,
-        antialias? :boolean, preserveDrawingBuffer? : boolean,
-        forceFXAA? : boolean, powerPreference? : string, webSocketServerName? : string)
-    {        
+    constructor(width: number, height: number, webSocketServerName? : string)
+    {
+        // Get preserveDrawingBuffer flag from webstorage
+        const preserveDrawingBuffer : boolean = localStorage.getItem("preserveDrawingBuffer")==="true"?true:false;        
+        
         // Call PIXI.Application
-        super({width:width, height:height, clearBeforeRender:clearBeforeReneder, transparent : transparent, 
-        antialias : antialias, preserveDrawingBuffer:preserveDrawingBuffer, forceFXAA: forceFXAA, powerPreference : powerPreference});
+        super({width:width, height:height, preserveDrawingBuffer});
            
         // Initialize websocket instances
         this.initializeWebSocketInstances();
