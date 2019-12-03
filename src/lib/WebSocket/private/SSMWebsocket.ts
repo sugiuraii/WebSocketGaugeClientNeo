2c24da0cdf0043a0cf1d5ce5a8b29a3b02ff292c
sz2 <sugiura.k.nigo@gmail.com>
2019-12-03 23:25:21 +0900

    Create SSMWebsocket.ts

--- src/lib/WebSocket/private/SSMWebsocket.ts
+++ src/lib/WebSocket/private/SSMWebsocket.ts
@@ -21,3 +21,168 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
+import {VALInterpolationBuffer} from "./utils/Interpolation";
+import * as JSONFormats from "./JSONFormats";
+import {WebsocketCommon} from "./WebsocketCommon";
+import {ReadModeCode} from "./parameterCode/ReadModeCode";
+import {SSMParameterCode} from "./parameterCode/SSMParameterCode";
+import {SSMSwitchCode} from "./parameterCode/SSMSwitchCode";
+import { EnumUtils } from "../../EnumUtils";
+
+export class SSMWebsocket extends WebsocketCommon
+{
+    private onVALPacketReceived : (intervalTime : number, val:{[code : string] : string}) => void;
+
+    //Internal state
+    private valPacketPreviousTimeStamp : number;
+    private valPacketIntervalTime : number;
+
+    //Interpolate value buffer
+    private interpolateBuffers: {[code: string]: VALInterpolationBuffer} = {};
+    //Switch data buffer    
+    private switchFlagBuffers: {[code: string] : boolean} = {};
+    
+    constructor()
+    {
+        super();
+        this.modePrefix = "SSM";
+        this.valPacketPreviousTimeStamp = window.performance.now();
+        this.valPacketIntervalTime = 0;
+    }
+
+    private checkInterpolateBufferAndCreateIfEmpty(code: string): void
+    {
+        if(!(code in this.interpolateBuffers))
+            this.interpolateBuffers[code] = new VALInterpolationBuffer();            
+    }
+
+    private checkSwitchFlagBuffersAndCreateIfEmpty(code: string): void
+    {
+        if(!(code in this.switchFlagBuffers))
+            this.switchFlagBuffers[code] = false;            
+    }
+
+    public getVal(code : SSMParameterCode, timestamp : number) : number
+    {
+        const codeStr = SSMParameterCode[code];
+        this.checkInterpolateBufferAndCreateIfEmpty(codeStr);
+        return this.interpolateBuffers[codeStr].getVal(timestamp);
+    }
+
+    public getRawVal(code : SSMParameterCode) : number
+    {
+        const codeStr = SSMParameterCode[code];
+        this.checkInterpolateBufferAndCreateIfEmpty(codeStr);
+        return this.interpolateBuffers[codeStr].getRawVal();
+    }
+    
+    public getSwitchFlag(code : SSMSwitchCode) : boolean
+    {
+        const codeStr = SSMSwitchCode[code];
+        this.checkSwitchFlagBuffersAndCreateIfEmpty[codeStr];
+        return this.switchFlagBuffers[codeStr];
+    }
+
+    public SendCOMRead(code: SSMParameterCode, readmode: ReadModeCode, flag: boolean): void
+    {
+        if (!this.IsConnetced)
+            return;
+
+        let sendCOMReadObj = new JSONFormats.SendCOMReadJSONMessage();
+        sendCOMReadObj.mode = this.modePrefix + "_COM_READ";
+        sendCOMReadObj.code = SSMParameterCode[code];
+        sendCOMReadObj.read_mode = ReadModeCode[readmode];
+        sendCOMReadObj.flag = flag;
+        const jsonstr = JSON.stringify(sendCOMReadObj);
+        this.WebSocket.send(jsonstr);
+    }
+
+    public SendSlowreadInterval(interval : number)
+    {
+        if (!this.IsConnetced)
+            return;
+
+        let sendSlowreadIntervalObj = new JSONFormats.SendSlowReadIntervalJSONMessage();
+        sendSlowreadIntervalObj.mode = this.modePrefix + "_SLOWREAD_INTERVAL";
+        sendSlowreadIntervalObj.interval = interval;
+        const jsonstr = JSON.stringify(sendSlowreadIntervalObj);
+        this.WebSocket.send(jsonstr);
+    }
+
+    private processVALJSONMessage(receivedJson: JSONFormats.StringVALJSONMessage) : void
+    {
+        //Update interval time
+        var nowTime = window.performance.now();
+        this.valPacketIntervalTime = nowTime - this.valPacketPreviousTimeStamp;
+        this.valPacketPreviousTimeStamp = nowTime;
+
+        // Invoke VALPacketReceived Event
+        if ( typeof(this.onVALPacketReceived) !== "undefined" )
+            this.onVALPacketReceived(this.valPacketIntervalTime, receivedJson.val);
+        
+        // Store value into interpolation buffers
+        for (let key in receivedJson.val)
+        {            
+            const valStr : string = receivedJson.val[key];
+            
+            if(EnumUtils.IsEnumContaninsKey(SSMParameterCode, key))
+            {
+                const val: number = Number(valStr);
+                // Register to interpolate buffer
+                this.checkInterpolateBufferAndCreateIfEmpty(key);
+                this.interpolateBuffers[key].setVal(val);
+            }
+            else if (EnumUtils.IsEnumContaninsKey(SSMSwitchCode, key))
+            {
+                let valFlag : boolean;
+                if(valStr.toLowerCase() === "true")
+                    valFlag = true;
+                else if(valStr.toLowerCase() === "false")
+                    valFlag = false;
+                else
+                    throw EvalError("Value of switch data is neither true nor false. value=" + valStr);
+                
+                this.switchFlagBuffers[key] = valFlag;                   
+            }
+            else
+                throw EvalError("Key of VAL message is not found in SSMParameterCode or SSMSwitchCode. key=" + key);
+        }
+    }
+    
+    private processERRJSONMessage(receivedJson: JSONFormats.ErrorJSONMessage)
+    {
+        if (typeof (this.OnERRPacketReceived) !== "undefined")
+            this.OnERRPacketReceived(receivedJson.msg);
+    }
+    
+    private processRESJSONMessage(receivedJson: JSONFormats.ResponseJSONMessage)
+    {
+        if(typeof (this.OnRESPacketReceived) !== "undefined")
+            this.OnRESPacketReceived(receivedJson.msg);
+    }
+    
+    protected parseIncomingMessage(msg : string) : void
+    {
+        const receivedJson : any = JSON.parse(msg);
+        const modeCode : string = (<JSONFormats.IJSONMessage>receivedJson).mode;
+        switch (modeCode)
+        {
+            case ("VAL") :
+                this.processVALJSONMessage(<JSONFormats.StringVALJSONMessage>receivedJson);
+                break;
+            case("ERR"):
+                this.processERRJSONMessage(<JSONFormats.ErrorJSONMessage>receivedJson);
+                break;
+            case("RES"):
+                this.processRESJSONMessage(<JSONFormats.ResponseJSONMessage>receivedJson);
+                break;
+            default:
+                this.OnWebsocketError("Unknown mode packet received. " + msg);
+        };
+    }
+
+    public get OnVALPacketReceived() {return this.onVALPacketReceived};
+    public set OnVALPacketReceived(func) {this.onVALPacketReceived = func };
+    public get VALPacketIntervalTime(): number { return this.valPacketIntervalTime; }
+}
