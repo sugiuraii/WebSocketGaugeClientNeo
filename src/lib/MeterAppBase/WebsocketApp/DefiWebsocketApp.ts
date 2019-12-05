b7cdd78b8218e13bacc28bbb3ad51d6a87926277
sz2 <sugiura.k.nigo@gmail.com>
2019-12-06 00:45:06 +0900

    Partially create DefiWebsocketApp.ts

---
+++ src/lib/MeterAppBase/WebsocketApp/DefiWebsocketApp.ts
@@ -0,0 +1,83 @@
+/* 
+ * The MIT License
+ *
+ * Copyright 2017 kuniaki.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import {DefiCOMWebsocket, DefiParameterCode} from "../../WebSocket/WebSocketCommunication";
+import {WebstorageHandler} from "../Webstorage/WebstorageHandler";
+import {ILogWindow} from "../ControlPanelParts/LogWindow";
+
+ export class DefiWebsocketApp
+ {
+   private readonly logPrefix = "Defi";
+   private readonly WEBSOCKET_CHECK_INTERVAL = 1000;
+   private readonly WAITTIME_BEFORE_SENDWSSEND = 3000;
+   private readonly WAITTIME_BEFORE_RECONNECT = 5000;
+   
+   private readonly defiWS: DefiCOMWebsocket;
+   private readonly defiParameterCodeList: {code: DefiParameterCode}[] = new Array();
+   private readonly loggerWindow : ILogWindow;
+
+   public get DefiWS() {return this.defiWS};
+   public get ParameterCodeList() { return this.defiParameterCodeList };
+
+   private readonly webSocketServerURL : string;
+
+   constructor(serverurl : string, loggerWindow : ILogWindow)
+   {
+      this.defiWS = new DefiCOMWebsocket();
+      this.loggerWindow = loggerWindow;
+
+      this.defiWS.OnWebsocketError = (message: string) => this.loggerWindow.appendLog(this.logPrefix + " websocket error : " + message);
+   }
+
+   private connectWebSocket()
+   {
+       const LogWindow = this.loggerWindow;
+       const webSocketObj = this.defiWS;
+       const logPrefix = this.logPrefix;
+
+       webSocketObj.OnWebsocketOpen = () =>
+       {
+           LogWindow.appendLog(logPrefix + " is connected. SendWSSend/Interval after "  + this.WAITTIME_BEFORE_SENDWSSEND.toString() + " msec");
+           window.setTimeout( () => 
+           {
+               //SendWSSend
+               this.ParameterCodeList.forEach(item => webSocketObj.SendWSSend(item.code, true));
+               
+               //SendWSInterval from spinner
+               webSocketObj.SendWSInterval(WebstorageHandler.GetWSIntervalFromLocalStorage());
+               
+           }, this.WAITTIME_BEFORE_SENDWSSEND);
+       }
+       webSocketObj.OnWebsocketClose = () =>
+       {
+           LogWindow.appendLog(logPrefix + " is disconnected. Reconnect after " + this.WAITTIME_BEFORE_RECONNECT.toString() + "msec...");                
+           window.setTimeout(() => webSocketObj.Connect(), this.WAITTIME_BEFORE_RECONNECT);
+       }
+
+       LogWindow.appendLog(logPrefix + " connect...");
+       webSocketObj.Connect();
+   }
+
+
+ }
