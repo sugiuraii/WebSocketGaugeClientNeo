d9493cae3c162a6065a4cff1f7c4767b2aa5809b
sz2 <sugiura.k.nigo@gmail.com>
2019-12-17 01:06:03 +0900

    Modify LogModalDialog to implement ILogWindow

--- src/lib/MeterAppBase/bootstrapParts/LogModalDialog.ts
+++ src/lib/MeterAppBase/bootstrapParts/LogModalDialog.ts
@@ -23,8 +23,9 @@
  */
 
 import * as $ from 'jquery';
+import {ILogWindow} from '../WebsocketAppBackend/ILogWindow';
 
-export class LogModalDialog
+export class LogModalDialog implements ILogWindow
 {
     private writeDate = false;
     
