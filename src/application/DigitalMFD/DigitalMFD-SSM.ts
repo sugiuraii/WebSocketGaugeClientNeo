1f796b83ae4612191ddf9fa479903287963a10b1
sz2 <sugiura.k.nigo@gmail.com>
2019-12-27 01:01:39 +0900

    Modify DigitalMFD to apply new MeterApplication class.

--- src/application/DigitalMFD/DigitalMFD-SSM.ts
+++ src/application/DigitalMFD/DigitalMFD-SSM.ts
@@ -4,13 +4,13 @@
  * Copyright 2017 kuniaki.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
+ * of ws software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in
+ * The above copyright notice and ws permission notice shall be included in
  * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -21,158 +21,158 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
- 
-// This is required to webpack font/texture/html files
+
+// ws is required to webpack font/texture/html files
 /// <reference path="../../lib/webpackRequire.ts" />
 
 import * as PIXI from "pixi.js";
 
 //Import application base class
-import {MeterApplicationBase} from "../../lib/MeterAppBase/MeterApplicationBase";
+import { MeterApplication } from "../../lib/MeterAppBase/MeterApplication";
+import { MeterApplicationOption } from "../../lib/MeterAppBase/options/MeterApplicationOption";
 
 //Import meter parts
-import {BoostGaugePanel} from "../../parts/CircularGauges/FullCircularGaugePanel";
-import {AirFuelGaugePanel} from "../../parts/CircularGauges/FullCircularGaugePanel";
-import {WaterTempGaugePanel} from "../../parts/CircularGauges/SemiCircularGaugePanel";
-import {BatteryVoltageGaugePanel} from "../../parts/CircularGauges/SemiCircularGaugePanel";
-import {ThrottleGaugePanel} from "../../parts/CircularGauges/SemiCircularGaugePanel";
-import {DigiTachoPanel} from "../../parts/DigiTachoPanel/DigiTachoPanel";
-import {MilageGraphPanel} from "../../parts/GasMilageGraph/MilageGraph";
+import { BoostGaugePanel } from "../../parts/CircularGauges/FullCircularGaugePanel";
+import { AirFuelGaugePanel } from "../../parts/CircularGauges/FullCircularGaugePanel";
+import { WaterTempGaugePanel } from "../../parts/CircularGauges/SemiCircularGaugePanel";
+import { BatteryVoltageGaugePanel } from "../../parts/CircularGauges/SemiCircularGaugePanel";
+import { ThrottleGaugePanel } from "../../parts/CircularGauges/SemiCircularGaugePanel";
+import { DigiTachoPanel } from "../../parts/DigiTachoPanel/DigiTachoPanel";
+import { MilageGraphPanel } from "../../parts/GasMilageGraph/MilageGraph";
 
 //Import enumuator of parameter code
-import {SSMParameterCode} from "../../lib/WebSocket/WebSocketCommunication";
-import {SSMSwitchCode} from "../../lib/WebSocket/WebSocketCommunication";
-import {ReadModeCode} from "../../lib/WebSocket/WebSocketCommunication";
+import { SSMParameterCode } from "../../lib/WebSocket/WebSocketCommunication";
+import { SSMSwitchCode } from "../../lib/WebSocket/WebSocketCommunication";
+import { ReadModeCode } from "../../lib/WebSocket/WebSocketCommunication";
+
+import {calculateGearPosition} from "../../lib/MeterAppBase/utils/CalculateGearPosition";
 
 //For including entry point html file in webpack
 require("./DigitalMFD-SSM.html");
 
-window.onload = function()
-{
-    const meterapp = new DigitalMFD_SSM(1200, 600);
-    meterapp.run();
+window.onload = function () {
+    const meterapp = new DigitalMFD_SSM();
+    meterapp.Start();
 }
 
-class DigitalMFD_SSM extends MeterApplicationBase
-{
-    protected setWebSocketOptions()
-    {
-        this.IsSSMWSEnabled = true;
-        this.IsFUELTRIPWSEnabled = true;
-        
-        this.registerSSMParameterCode(SSMParameterCode.Engine_Speed, ReadModeCode.SLOWandFAST);
-        this.registerSSMParameterCode(SSMParameterCode.Manifold_Absolute_Pressure, ReadModeCode.SLOWandFAST);
-        this.registerSSMParameterCode(SSMParameterCode.Vehicle_Speed, ReadModeCode.SLOWandFAST);
-        this.registerSSMParameterCode(SSMParameterCode.Coolant_Temperature, ReadModeCode.SLOW);
-        this.registerSSMParameterCode(SSMParameterCode.Battery_Voltage, ReadModeCode.SLOW);
-        this.registerSSMParameterCode(SSMParameterCode.Throttle_Opening_Angle, ReadModeCode.SLOWandFAST);
-        this.registerSSMParameterCode(SSMParameterCode.Air_Fuel_Sensor_1, ReadModeCode.SLOWandFAST);
-        this.registerSSMParameterCode(SSMSwitchCode.getNumericCodeFromSwitchCode(SSMSwitchCode.Neutral_Position_Switch), ReadModeCode.SLOWandFAST);
-        
-        this.FUELTRIPSectSpan = 300;
-        this.FUELTRIPSectStoreMax = 6;
-    }
-    
-    protected setTextureFontPreloadOptions()
-    {
-        this.registerWebFontFamilyNameToPreload(BoostGaugePanel.RequestedFontFamily);
-        this.registerWebFontFamilyNameToPreload(WaterTempGaugePanel.RequestedFontFamily);
-        this.registerWebFontFamilyNameToPreload(DigiTachoPanel.RequestedFontFamily);
-        this.registerWebFontFamilyNameToPreload(MilageGraphPanel.RequestedFontFamily);
-        
-        this.registerWebFontCSSURLToPreload(BoostGaugePanel.RequestedFontCSSURL);
-        this.registerWebFontCSSURLToPreload(WaterTempGaugePanel.RequestedFontCSSURL);
-        this.registerWebFontCSSURLToPreload(DigiTachoPanel.RequestedFontCSSURL);
-        this.registerWebFontCSSURLToPreload(MilageGraphPanel.RequestedFontCSSURL);
-        
-        this.registerTexturePathToPreload(BoostGaugePanel.RequestedTexturePath);
-        this.registerTexturePathToPreload(WaterTempGaugePanel.RequestedTexturePath);
-        this.registerTexturePathToPreload(DigiTachoPanel.RequestedTexturePath);
-        this.registerTexturePathToPreload(MilageGraphPanel.RequestedTexturePath);
-    }
-    
-    protected setPIXIMeterPanel()
-    {        
-        const digiTachoPanel = new DigiTachoPanel();
-        digiTachoPanel.position.set(0,0);
-        
-        const milagePanel = new MilageGraphPanel();
-        milagePanel.position.set(0,300);
-        milagePanel.scale.set(0.94,0.94);
-        
-        const boostPanel = new BoostGaugePanel();
-        boostPanel.position.set(600,0);
-        boostPanel.scale.set(0.751,0.751);
-        
-        const airFuelPanel = new AirFuelGaugePanel();
-        airFuelPanel.position.set(600,310);
-        airFuelPanel.scale.set(0.751, 0.751);
-        
-        const waterTempPanel = new WaterTempGaugePanel();
-        waterTempPanel.position.set(900,0);
-        waterTempPanel.scale.set(0.68);
-        
-        const voltagePanel = new BatteryVoltageGaugePanel();
-        voltagePanel.position.set(900,200);
-        voltagePanel.scale.set(0.68);
-        
-        const throttlePanel = new ThrottleGaugePanel();
-        throttlePanel.position.set(900,400);
-        throttlePanel.scale.set(0.68);
-        
-        this.stage.addChild(digiTachoPanel);
-        this.stage.addChild(milagePanel);
-        this.stage.addChild(boostPanel);
-        this.stage.addChild(airFuelPanel);
-        this.stage.addChild(waterTempPanel);
-        this.stage.addChild(voltagePanel);
-        this.stage.addChild(throttlePanel);
-        
-        this.ticker.add(() => 
-        {
-            const timestamp = this.ticker.lastTime;
-            const tacho = this.SSMWS.getVal(SSMParameterCode.Engine_Speed, timestamp);
-            const speed = this.SSMWS.getVal(SSMParameterCode.Vehicle_Speed, timestamp);
-            const neutralSw = this.SSMWS.getSwitchFlag(SSMSwitchCode.Neutral_Position_Switch);
-            const gearPos = this.calculateGearPosition(tacho, speed, neutralSw);
+class DigitalMFD_SSM {
+    public Start() {
+        const appOption = new MeterApplicationOption();
+        appOption.width = 1200;
+        appOption.height = 600;
+        appOption.PreloadResource.WebFontFamiliyName.addall(BoostGaugePanel.RequestedFontFamily);
+        appOption.PreloadResource.WebFontFamiliyName.addall(WaterTempGaugePanel.RequestedFontFamily);
+        appOption.PreloadResource.WebFontFamiliyName.addall(DigiTachoPanel.RequestedFontFamily);
+        appOption.PreloadResource.WebFontFamiliyName.addall(MilageGraphPanel.RequestedFontFamily);
+        appOption.PreloadResource.WebFontCSSURL.addall(BoostGaugePanel.RequestedFontCSSURL);
+        appOption.PreloadResource.WebFontCSSURL.addall(WaterTempGaugePanel.RequestedFontCSSURL);
+        appOption.PreloadResource.WebFontCSSURL.addall(DigiTachoPanel.RequestedFontCSSURL);
+        appOption.PreloadResource.WebFontCSSURL.addall(MilageGraphPanel.RequestedFontCSSURL);
+        appOption.PreloadResource.TexturePath.addall(BoostGaugePanel.RequestedTexturePath);
+        appOption.PreloadResource.TexturePath.addall(WaterTempGaugePanel.RequestedTexturePath);
+        appOption.PreloadResource.TexturePath.addall(DigiTachoPanel.RequestedTexturePath);
+        appOption.PreloadResource.TexturePath.addall(MilageGraphPanel.RequestedTexturePath);
+
+        appOption.WebsocketEnableFlag.SSM = true;
+        appOption.WebsocketEnableFlag.FUELTRIP = true;
+        appOption.FUELTRIPWebsocketOption.FUELTRIPSectSpan = 300;
+        appOption.FUELTRIPWebsocketOption.FUELTRIPSectStoreMax = 6;
+
+        appOption.ParameterCode.SSM.addall({ code: SSMParameterCode.Engine_Speed, readmode: ReadModeCode.SLOWandFAST });
+        appOption.ParameterCode.SSM.addall({ code: SSMParameterCode.Manifold_Absolute_Pressure, readmode: ReadModeCode.SLOWandFAST });
+        appOption.ParameterCode.SSM.addall({ code: SSMParameterCode.Vehicle_Speed, readmode: ReadModeCode.SLOWandFAST });
+        appOption.ParameterCode.SSM.addall({ code: SSMParameterCode.Coolant_Temperature, readmode: ReadModeCode.SLOW });
+        appOption.ParameterCode.SSM.addall({ code: SSMParameterCode.Battery_Voltage, readmode: ReadModeCode.SLOW });
+        appOption.ParameterCode.SSM.addall({ code: SSMParameterCode.Throttle_Opening_Angle, readmode: ReadModeCode.SLOWandFAST });
+        appOption.ParameterCode.SSM.addall({ code: SSMParameterCode.Air_Fuel_Sensor_1, readmode: ReadModeCode.SLOWandFAST });
+        appOption.ParameterCode.SSM.addall({ code: SSMSwitchCode.getNumericCodeFromSwitchCode(SSMSwitchCode.Neutral_Position_Switch), readmode: ReadModeCode.SLOWandFAST });
+
+        appOption.SetupPIXIMeterPanel = (app, ws) => {
             
-            const momentGasMilage = this.FUELTRIPWS.getMomentGasMilage(timestamp);
-            const gasMilage5min : number = this.FUELTRIPWS.getSectGasMilage(0);
-            const gasMilage10min : number = this.FUELTRIPWS.getSectGasMilage(1);
-            const gasMilage15min : number = this.FUELTRIPWS.getSectGasMilage(2);
-            const gasMilage20min : number = this.FUELTRIPWS.getSectGasMilage(3);
-            const gasMilage25min : number = this.FUELTRIPWS.getSectGasMilage(4);
-            const gasMilage30min : number = this.FUELTRIPWS.getSectGasMilage(5);
-            const totalGasMilage = this.FUELTRIPWS.getTotalGasMilage();
-            const totalFuel = this.FUELTRIPWS.getTotalGas();
-            const totalTrip = this.FUELTRIPWS.getTotalTrip();
-            
-            const boost = this.SSMWS.getVal(SSMParameterCode.Manifold_Absolute_Pressure, timestamp)  * 0.0101972 - 1 //convert kPa to kgf/cm2 and relative pressure;
-            const airFuelRatio = this.SSMWS.getVal(SSMParameterCode.Air_Fuel_Sensor_1, timestamp)*14;
-            const waterTemp = this.SSMWS.getRawVal(SSMParameterCode.Coolant_Temperature);
-            const batteryVolt = this.SSMWS.getRawVal(SSMParameterCode.Battery_Voltage);
-            const throttle = this.SSMWS.getVal(SSMParameterCode.Throttle_Opening_Angle, timestamp);
-            
-            digiTachoPanel.Speed = speed;
-            digiTachoPanel.Tacho = tacho;
-            digiTachoPanel.GearPos = gearPos;
-            
-            milagePanel.MomentGasMilage = momentGasMilage;
-            milagePanel.Trip = totalTrip;
-            milagePanel.Fuel = totalFuel;
-            milagePanel.GasMilage = totalGasMilage;
-            milagePanel.setSectGasMllage("5min", gasMilage5min);
-            milagePanel.setSectGasMllage("10min", gasMilage10min);
-            milagePanel.setSectGasMllage("15min", gasMilage15min);
-            milagePanel.setSectGasMllage("20min", gasMilage20min);
-            milagePanel.setSectGasMllage("25min", gasMilage25min);
-            milagePanel.setSectGasMllage("30min", gasMilage30min);
-            
-            boostPanel.Value = boost;
-            airFuelPanel.Value = airFuelRatio;
-            waterTempPanel.Value = waterTemp;
-            voltagePanel.Value = batteryVolt;
-            throttlePanel.Value = throttle;
-       });
+            const stage = app.stage;
+
+            const digiTachoPanel = new DigiTachoPanel();
+            digiTachoPanel.position.set(0, 0);
+
+            const milagePanel = new MilageGraphPanel();
+            milagePanel.position.set(0, 300);
+            milagePanel.scale.set(0.94, 0.94);
+
+            const boostPanel = new BoostGaugePanel();
+            boostPanel.position.set(600, 0);
+            boostPanel.scale.set(0.751, 0.751);
+
+            const airFuelPanel = new AirFuelGaugePanel();
+            airFuelPanel.position.set(600, 310);
+            airFuelPanel.scale.set(0.751, 0.751);
+
+            const waterTempPanel = new WaterTempGaugePanel();
+            waterTempPanel.position.set(900, 0);
+            waterTempPanel.scale.set(0.68);
+
+            const voltagePanel = new BatteryVoltageGaugePanel();
+            voltagePanel.position.set(900, 200);
+            voltagePanel.scale.set(0.68);
+
+            const throttlePanel = new ThrottleGaugePanel();
+            throttlePanel.position.set(900, 400);
+            throttlePanel.scale.set(0.68);
+
+            stage.addChild(digiTachoPanel);
+            stage.addChild(milagePanel);
+            stage.addChild(boostPanel);
+            stage.addChild(airFuelPanel);
+            stage.addChild(waterTempPanel);
+            stage.addChild(voltagePanel);
+            stage.addChild(throttlePanel);
+
+            app.ticker.add(() => {
+                const timestamp = app.ticker.lastTime;
+                const tacho = ws.SSMWS.getVal(SSMParameterCode.Engine_Speed, timestamp);
+                const speed = ws.SSMWS.getVal(SSMParameterCode.Vehicle_Speed, timestamp);
+                const neutralSw = ws.SSMWS.getSwitchFlag(SSMSwitchCode.Neutral_Position_Switch);
+                const gearPos = calculateGearPosition(tacho, speed, neutralSw);
+
+                const momentGasMilage = ws.FUELTRIPWS.getMomentGasMilage(timestamp);
+                const gasMilage5min: number = ws.FUELTRIPWS.getSectGasMilage(0);
+                const gasMilage10min: number = ws.FUELTRIPWS.getSectGasMilage(1);
+                const gasMilage15min: number = ws.FUELTRIPWS.getSectGasMilage(2);
+                const gasMilage20min: number = ws.FUELTRIPWS.getSectGasMilage(3);
+                const gasMilage25min: number = ws.FUELTRIPWS.getSectGasMilage(4);
+                const gasMilage30min: number = ws.FUELTRIPWS.getSectGasMilage(5);
+                const totalGasMilage = ws.FUELTRIPWS.getTotalGasMilage();
+                const totalFuel = ws.FUELTRIPWS.getTotalGas();
+                const totalTrip = ws.FUELTRIPWS.getTotalTrip();
+
+                const boost = ws.SSMWS.getVal(SSMParameterCode.Manifold_Absolute_Pressure, timestamp) * 0.0101972 - 1 //convert kPa to kgf/cm2 and relative pressure;
+                const airFuelRatio = ws.SSMWS.getVal(SSMParameterCode.Air_Fuel_Sensor_1, timestamp) * 14;
+                const waterTemp = ws.SSMWS.getRawVal(SSMParameterCode.Coolant_Temperature);
+                const batteryVolt = ws.SSMWS.getRawVal(SSMParameterCode.Battery_Voltage);
+                const throttle = ws.SSMWS.getVal(SSMParameterCode.Throttle_Opening_Angle, timestamp);
+
+                digiTachoPanel.Speed = speed;
+                digiTachoPanel.Tacho = tacho;
+                digiTachoPanel.GearPos = gearPos;
+
+                milagePanel.MomentGasMilage = momentGasMilage;
+                milagePanel.Trip = totalTrip;
+                milagePanel.Fuel = totalFuel;
+                milagePanel.GasMilage = totalGasMilage;
+                milagePanel.setSectGasMllage("5min", gasMilage5min);
+                milagePanel.setSectGasMllage("10min", gasMilage10min);
+                milagePanel.setSectGasMllage("15min", gasMilage15min);
+                milagePanel.setSectGasMllage("20min", gasMilage20min);
+                milagePanel.setSectGasMllage("25min", gasMilage25min);
+                milagePanel.setSectGasMllage("30min", gasMilage30min);
+
+                boostPanel.Value = boost;
+                airFuelPanel.Value = airFuelRatio;
+                waterTempPanel.Value = waterTemp;
+                voltagePanel.Value = batteryVolt;
+                throttlePanel.Value = throttle;
+            });
+        };
+        const app = new MeterApplication(appOption);
+        app.Run();
     }
 }
