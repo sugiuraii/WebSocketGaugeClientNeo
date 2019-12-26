151ae340db6ae7a3adc4811ef3440ec9588631bd
sz2 <sugiura.k.nigo@gmail.com>
2019-12-26 22:55:13 +0900

    Fix AnalogMeterCluster application class for new MeterApplication definition.

--- src/application/AnalogMeterCluster/AnalogMeterCluster-AssettoCorsaSHM.ts
+++ src/application/AnalogMeterCluster/AnalogMeterCluster-AssettoCorsaSHM.ts
@@ -21,83 +21,81 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
- 
+
 // This is required to webpack font/texture/html files 
 /// <reference path="../../lib/webpackRequire.ts" />
 
 import * as PIXI from "pixi.js";
 
 //Import application base class
-import {MeterApplicationBase} from "../../lib/MeterAppBase/MeterApplicationBase";
+import { MeterApplication } from "../../lib/MeterAppBase/MeterApplication";
+import { MeterApplicationOption } from "../../lib/MeterAppBase/options/MeterApplicationOption";
 
 //Import meter parts
-import {AnalogMeterCluster} from "../../parts/AnalogMeterCluster/AnalogMeterCluster";
+import { AnalogMeterCluster } from "../../parts/AnalogMeterCluster/AnalogMeterCluster";
+
 
 //Import enumuator of parameter code
-import {AssettoCorsaSHMPhysicsParameterCode, AssettoCorsaSHMNumericalVALCode} from "../../lib/WebSocket/WebSocketCommunication";
-import {AssettoCorsaSHMGraphicsParameterCode} from "../../lib/WebSocket/WebSocketCommunication";
-import {AssettoCorsaSHMStaticInfoParameterCode} from "../../lib/WebSocket/WebSocketCommunication";
+import { AssettoCorsaSHMPhysicsParameterCode, AssettoCorsaSHMNumericalVALCode } from "../../lib/WebSocket/WebSocketCommunication";
+import { AssettoCorsaSHMGraphicsParameterCode } from "../../lib/WebSocket/WebSocketCommunication";
+import { AssettoCorsaSHMStaticInfoParameterCode } from "../../lib/WebSocket/WebSocketCommunication";
 
 //For including entry point html file in webpack
 require("./AnalogMeterCluster-AssettoCorsaSHM.html");
 
-window.onload = function()
-{
-    const meterapp = new AnalogMeterCluster_AssettoCorsaSHM(1100, 600);
-    meterapp.run();
+window.onload = function () {
+    const meterapp = new AnalogMeterCluster_AssettoCorsaSHM();
+    meterapp.Start();
 }
 
-class AnalogMeterCluster_AssettoCorsaSHM extends MeterApplicationBase
-{
-    protected setWebSocketOptions()
-    {
-        this.IsAssettoCorsaWSEnabled = true;
-        
-        this.registerAssettoCorsaPhysicsParameterCode(AssettoCorsaSHMPhysicsParameterCode.Rpms);
-        this.registerAssettoCorsaPhysicsParameterCode(AssettoCorsaSHMPhysicsParameterCode.SpeedKmh);
-        this.registerAssettoCorsaPhysicsParameterCode(AssettoCorsaSHMPhysicsParameterCode.ManifoldPressure);
-        this.registerAssettoCorsaPhysicsParameterCode(AssettoCorsaSHMPhysicsParameterCode.Gear);
-    }
-    
-    protected setTextureFontPreloadOptions()
-    {
-        this.registerWebFontFamilyNameToPreload(AnalogMeterCluster.RequestedFontFamily);
-        this.registerWebFontCSSURLToPreload(AnalogMeterCluster.RequestedFontCSSURL);
-        this.registerTexturePathToPreload(AnalogMeterCluster.RequestedTexturePath);
-    }
-    
-    protected setPIXIMeterPanel() : void
-    {
-        const meterCluster = new AnalogMeterCluster();
-        this.stage.addChild(meterCluster);
+class AnalogMeterCluster_AssettoCorsaSHM {
+    public Start() {
+        const appOption = new MeterApplicationOption();
+        appOption.width = 1100;
+        appOption.height = 600;
+        appOption.PreloadResource.WebFontFamiliyName.addall(AnalogMeterCluster.RequestedFontFamily);
+        appOption.PreloadResource.WebFontCSSURL.addall(AnalogMeterCluster.RequestedFontCSSURL);
+        appOption.PreloadResource.TexturePath.addall(AnalogMeterCluster.RequestedTexturePath);
 
-        this.ticker.add(() => {
-            const timestamp = this.ticker.lastTime;
-            const tacho = this.AssettoCorsaWS.getVal(AssettoCorsaSHMNumericalVALCode.Rpms, timestamp);
-            const boost = this.AssettoCorsaWS.getVal(AssettoCorsaSHMNumericalVALCode.ManifoldPressure, timestamp) * 1.01972 //convert kPa to kgf/cm2 and relative pressure;
-            const speed = this.AssettoCorsaWS.getVal(AssettoCorsaSHMNumericalVALCode.SpeedKmh, timestamp);
-            const waterTemp = 95;
-            const trip = 100;
-            const fuel = 20;
-            const gasMilage = 10;
-            
-            const gearPos = this.AssettoCorsaWS.getRawVal(AssettoCorsaSHMNumericalVALCode.Gear);
-            
-            meterCluster.Tacho = tacho;
-            meterCluster.Boost = boost; 
-            meterCluster.Speed = speed;
-            meterCluster.WaterTemp = waterTemp;
+        appOption.WebsocketEnableFlag.AssettoCorsaSHM = true;
+        appOption.ParameterCode.AssettoCorsaPhysics.addall(AssettoCorsaSHMPhysicsParameterCode.Rpms);
+        appOption.ParameterCode.AssettoCorsaPhysics.addall(AssettoCorsaSHMPhysicsParameterCode.SpeedKmh);
+        appOption.ParameterCode.AssettoCorsaPhysics.addall(AssettoCorsaSHMPhysicsParameterCode.ManifoldPressure);
+        appOption.ParameterCode.AssettoCorsaPhysics.addall(AssettoCorsaSHMPhysicsParameterCode.Gear);
 
-            if(gearPos === 0)
-                meterCluster.GearPos = "R";
-            else if(gearPos === 1)
-                meterCluster.GearPos = "N";
-            else
-                meterCluster.GearPos = (gearPos-1).toString();
-            
-            meterCluster.Trip = trip;
-            meterCluster.Fuel = fuel;
-            meterCluster.GasMilage = gasMilage;
-        });
+        appOption.SetupPIXIMeterPanel = (app, ws) => {
+            const stage = app.stage;
+            const meterCluster = new AnalogMeterCluster();
+            stage.addChild(meterCluster);
+
+            app.ticker.add(() => {
+                const timestamp = app.ticker.lastTime;
+                const tacho = ws.AssettoCorsaWS.getVal(AssettoCorsaSHMNumericalVALCode.Rpms, timestamp);
+                const boost = ws.AssettoCorsaWS.getVal(AssettoCorsaSHMNumericalVALCode.ManifoldPressure, timestamp) * 1.01972 //convert kPa to kgf/cm2 and relative pressure;
+                const speed = ws.AssettoCorsaWS.getVal(AssettoCorsaSHMNumericalVALCode.SpeedKmh, timestamp);
+                const waterTemp = 95;
+                const trip = 100;
+                const fuel = 20;
+                const gasMilage = 10;
+
+                const gearPos = ws.AssettoCorsaWS.getRawVal(AssettoCorsaSHMNumericalVALCode.Gear);
+
+                meterCluster.Tacho = tacho;
+                meterCluster.Boost = boost;
+                meterCluster.Speed = speed;
+                meterCluster.WaterTemp = waterTemp;
+
+                if (gearPos === 0)
+                    meterCluster.GearPos = "R";
+                else if (gearPos === 1)
+                    meterCluster.GearPos = "N";
+                else
+                    meterCluster.GearPos = (gearPos - 1).toString();
+
+                meterCluster.Trip = trip;
+                meterCluster.Fuel = fuel;
+                meterCluster.GasMilage = gasMilage;
+            });
+        }
     }
 }
