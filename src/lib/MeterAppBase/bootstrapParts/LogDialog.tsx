e8debdaef27c67948c67a7ca323ebbf4dec17967
sz2 <sugiura.k.nigo@gmail.com>
2020-10-31 17:23:17 +0900

    Create LogDialog.tsx

---
+++ src/lib/MeterAppBase/bootstrapParts/LogDialog.tsx
@@ -0,0 +1,51 @@
+/* 
+ * The MIT License
+ *
+ * Copyright 2017 kuniaki.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import React, { FunctionComponent } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+type LogDialogProps = 
+{
+    show : boolean,
+    log : string,
+    onClose: () => void;
+}
+
+const LogDialog: FunctionComponent<LogDialogProps> = (p) => {
+    return(
+        <Modal show={p.show} >
+        <Modal.Header closeButton onHide={p.onClose}>
+            <Modal.Title>Log</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            {p.log}
+        </Modal.Body>
+        <Modal.Footer>
+            <Button variant="secondary" onClick={p.onClose}>Close</Button>
+        </Modal.Footer>
+    </Modal>
+    );
+}
+
+export default LogDialog;
