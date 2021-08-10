c6f7afe6b77b139ed79a9ad268496bc0576254e5
sz2 <sugiura.k.nigo@gmail.com>
2021-08-11 02:15:04 +0900

    Test webpack tsconfig path plugin.

--- src/WebSocketTester/webpack.config.js
+++ src/WebSocketTester/webpack.config.js
@@ -25,6 +25,7 @@ const webpack = require('webpack');
 const path = require('path');
 const pjrootPath = path.resolve(__dirname, "../../")
 const outputPath = path.resolve(pjrootPath, "public_html/WebSocketTester");
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
     mode: "development",
@@ -45,7 +46,8 @@ module.exports = {
     },
     resolve: {
         // Add `.ts` and `.tsx` as a resolvable extension.
-        extensions: ['.ts', '.tsx', '.js']
+        extensions: ['.ts', '.tsx', '.js'],
+        plugins: [new TsconfigPathsPlugin({configFile: path.resolve(pjrootPath, "src/tsconfig.json")})]
     },
     module: {
         rules: [
@@ -53,10 +55,7 @@ module.exports = {
             {
                 test: /\.ts?$/,
                 use: {
-                    loader: 'ts-loader', 
-                    options: {
-                        configFile: path.resolve(pjrootPath, 'src/tsconfig.json'),
-                    }
+                    loader: 'ts-loader'
                 }
             },
             { test: /\.html$/, use: "file-loader?name=[name].[ext]"}
