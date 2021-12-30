aa56c18385f6dd60ffe867aee6eb524765e0bcb3
sz2 <sugiura.k.nigo@gmail.com>
2021-12-31 02:52:19 +0900

    Add MeterWidgetConfigPage renderer class.

---
+++ src/lib/MeterAppBase/reactParts/widgetSetting/MeterWidgetConfigPageRenderer.tsx
@@ -0,0 +1,59 @@
+/* 
+ * The MIT License
+ *
+ * Copyright 2017 sz2.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { MeterWidgetConfigPage } from "./MeterWidgetConfigPage";
+
+export class MeterWidgetConfigPageRenderer
+{
+    private readonly BOOTSTRAP_CSS_FILENAME = "bootstrap.min.css";
+
+    private loadBootStrapCSS() 
+    {
+        const head = document.getElementsByTagName('head')[0];
+        const link = document.createElement('link');
+        link.setAttribute('rel', 'stylesheet');
+        link.setAttribute('type', 'text/css');
+        link.setAttribute('href', this.BOOTSTRAP_CSS_FILENAME);
+        head.appendChild(link);
+    }
+
+    public render(baseURL: string, previewHeight?:string, previewWidth?:string)
+    {
+        const rootElement = document.createElement('div');
+        this.loadBootStrapCSS();
+
+        ReactDOM.render(
+            <>
+                <MeterWidgetConfigPage  previewHeight={previewHeight} previewWidth={previewWidth}
+                                        baseURL={baseURL} 
+                                        default={{forceCanvas:false, wsInterval:0}} />
+            </>
+            , rootElement);    
+
+        // Add react components to html body
+        document.body.appendChild(rootElement);
+    }
+}
