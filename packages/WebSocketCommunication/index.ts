3daa485f1b05f7ae401e4158d4fa29e9e06f2292
sz2 <sugiura.k.nigo@gmail.com>
2022-10-09 12:56:43 +0900

    Move websocket communication sources.

---
+++ packages/WebSocketCommunication/index.ts
@@ -0,0 +1,44 @@
+/* 
+ * The MIT License
+ *
+ * Copyright 2017 sz2.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+export {WebsocketCommon} from "./WebsocketCommon";
+export {DefiCOMWebsocket} from "./DefiCOMWebsocket";
+export {ArduinoCOMWebsocket} from "./ArduinoCOMWebsocket";
+export {SSMWebsocket} from "./SSMWebsocket";
+export {ELM327COMWebsocket} from "./ELM327COMWebsocket";
+export {FUELTRIPWebsocket} from "./FUELTRIPWebsocket";
+export {AssettoCorsaSHMWebsocket} from "./AssetoCorsaSHMWebSocket";
+
+export {DefiParameterCode} from "./parameterCode/DefiParameterCode";
+export {ArduinoParameterCode} from "./parameterCode/ArduinoParameterCode";
+export {SSMParameterCode} from "./parameterCode/SSMParameterCode";
+export {SSMSwitchCode} from "./parameterCode/SSMSwitchCode";
+export {OBDIIParameterCode} from "./parameterCode/OBDIIParameterCode";
+export {AssettoCorsaSHMPhysicsParameterCode} from "./parameterCode/AssettoCorsaSHMParameterCode";
+export {AssettoCorsaSHMGraphicsParameterCode} from "./parameterCode/AssettoCorsaSHMParameterCode";
+export {AssettoCorsaSHMStaticInfoParameterCode} from "./parameterCode/AssettoCorsaSHMParameterCode";
+export {AssettoCorsaSHMNumericalVALCode} from "./parameterCode/AssettoCorsaSHMParameterCode";
+export {AssettoCorsaSHMStringVALCode} from "./parameterCode/AssettoCorsaSHMParameterCode";
+export {ReadModeCode} from "./parameterCode/ReadModeCode";
+
