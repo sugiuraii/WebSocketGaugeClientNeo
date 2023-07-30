e7db5b5ec591eea40671ce7322fd8d51eb91ff83
sz2 <sugiura.k.nigo@gmail.com>
2023-07-30 21:44:55 +0900

    Create index.ts

---
+++ packages/websocket-gauge-client-communication-service/index.ts
@@ -0,0 +1,39 @@
+/* 
+ * The MIT License
+ *
+ * Copyright 2017 sz2.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+export { * in the Software without restriction, including without limitation the rig} from "./WebsocketClientService/ * in the Software without restriction, including without limitation the rig"
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+export {ArduinoWebsocketClientService} from "./WebsocketClientService/ArduinoWebsocketClientService"
+export {DefiWebsocketClientService} from "./WebsocketClientService/DefiWebsocketClientService"
+export {FUELTRIPWebsocketClientService} from "./WebsocketClientService/FUELTRIPWebsocketClientService"
+export {WebsocketClientService} from "./WebsocketClientService/WebsocketClientService"
+export {WebsocketState} from "./WebsocketClientService/WebsocketState"
+export {AssettoCorsaSHMWebsocketClientService} from "./WebsocketClientService/AssettoCorsaSHMWebsocketService"
+export {ELM327WebsocketClientService} from "./WebsocketClientService/ELM327WebsocketClientService"
+export {SSMWebsocketClientService} from "./WebsocketClientService/SSMWebsocketClientService"
+export {WebsocketConnectionStatus} from "./WebsocketClientService/WebsocketConnectionStatus"
+
+export {WebsocketParameterCode} from "./WebsocketServiceCollection/WebsocketParameterCode"
+export {WebsocketObjectCollectionOption, WebsocketServiceCollection} from "./WebsocketServiceCollection/WebsocketServiceCollection"
+export {WebsocketServiceMapFactory, DefaultDefiMap} from "./WebsocketServiceCollection/WebsocketServiceMapFactory"
+export {WebsocketServiceMapEntry, WebsocketServiceMapper} from "./WebsocketServiceCollection/WebsocketServiceMapper"
+
