5a7d77514d7b309ead46b5afa9f8ff6699389bd2
sz2 <sugiura.k.nigo@gmail.com>
2024-02-03 23:46:18 +0900

    Fix FullcircularGauge.svg and crate pngexport.sh

---
+++ src/design/svgs/CircularGauge/pngexport.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Set command/path of inkscape
+#inkscape_cmd=inkscape
+
+# Set svg file name
+svg_filename=FullcircularGauge.svg
+
+# Set list of ids to export
+id_list=(\
+ "layer_back"\
+ "layer_grid"\
+)
+
+for id in "${id_list[@]}"
+do
+  $inkscape_cmd --export-type="png" $svg_filename --export-id="$id" --export-id-only --export-area-page
+done
+
+$inkscape_cmd --export-type="png" $svg_filename --export-id=layer_valuebar --export-id-only
+$inkscape_cmd --export-type="png" $svg_filename --export-id=layer_shaft --export-id-only
+
+$inkscape_cmd --export-type="png" $svg_filename --export-id=layer_redzone_bar --export-id-only
+$inkscape_cmd --export-type="png" $svg_filename --export-id=layer_yellowzone_bar --export-id-only
+$inkscape_cmd --export-type="png" $svg_filename --export-id=layer_greenzone_bar --export-id-only
