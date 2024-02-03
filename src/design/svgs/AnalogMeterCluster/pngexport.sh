023d8c426a22cb4d4bcd66bd46f92b5ed17f4670
sz2 <sugiura.k.nigo@gmail.com>
2024-02-03 18:20:03 +0900

    Code pngexport.sh

--- src/design/svgs/AnalogMeterCluster/pngexport.sh
+++ src/design/svgs/AnalogMeterCluster/pngexport.sh
@@ -11,6 +11,21 @@ id_list=(\
  "layer_tacho_base"\
  "layer_tacho_red"\
  "layer_tacho_lcd_base"\
+ "layer_tacho_lcd_bar"\
+ "layer_tacho_text_lcd_fixed"\
+ "layer_tacho_tachotext"\
+ "layer_tacho_needle"\
+ "layer_tacho_needlecap"\
+ "layer_speed_base"\
+ "layer_speed_text"\
+ "layer_speed_lcdbase"\
+ "layer_speed_lcdtext_fixed"\
+ "layer_speed_lcdbar"\
+ "layer_speed_needle"\
+ "layer_speed_needlecap"\
+ "layer_boost_base"\
+ "layer_boost_text"\
+ "layer_boost_needle"\
 )
 
 for id in "${id_list[@]}"
