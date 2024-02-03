660680105a9e6b10fdfa9becb76d49f3f1f42b98
sz2 <sugiura.k.nigo@gmail.com>
2024-02-03 23:06:27 +0900

    Fix DigiTachoMeter.svg, and create pngexport.sh.

---
+++ src/design/svgs/DigiTachoMeter/pngexport.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Set command/path of inkscape
+#inkscape_cmd=inkscape
+
+# Set svg file name
+svg_filename=DigiTachoMeter.svg
+
+# Set list of ids to export
+id_list=(\
+ "layer_digitachometer_back"\
+ "layer_digitachometer_grid"\
+ "layer_digitachometer_text"\
+)
+
+for id in "${id_list[@]}"
+do
+  $inkscape_cmd --export-type="png" $svg_filename --export-id="$id" --export-id-only --export-area-page
+done
+
+$inkscape_cmd --export-type="png" $svg_filename --export-id=layer_digitachometer_led_dark --export-id-only
