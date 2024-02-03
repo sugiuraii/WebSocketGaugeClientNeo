dafc6bf22e7c791b5fc03283c4b93160bf05b4fa
sz2 <sugiura.k.nigo@gmail.com>
2024-02-03 23:56:13 +0900

    Fix SemiCircularGauge.svg and create export.sh.

---
+++ src/design/svgs/CircularGauge/pngexport_semicircular.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Set command/path of inkscape
+#inkscape_cmd=inkscape
+
+# Set svg file name
+svg_filename=SemiCircularGauge.svg
+
+# Set list of ids to export
+id_list=(\
+ "layer_back"\
+ "layer_grid"\
+)
+
+for id in "${id_list[@]}"
+do
+  $inkscape_cmd --export-type="png" $svg_filename --export-id="$id" --export-id-only --export-area-page
+done
+
+$inkscape_cmd --export-type="png" $svg_filename --export-id=layer_valuebar --export-id-only
+
+$inkscape_cmd --export-type="png" $svg_filename --export-id=layer_redzone_bar --export-id-only
