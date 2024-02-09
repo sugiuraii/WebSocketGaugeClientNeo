1c3347652c52bb2b60f4cf96b021a447da402d50
sz2 <sugiura.k.nigo@gmail.com>
2024-02-09 17:48:55 +0900

    create font download script

---
+++ src/design/fonts/download_ttfs.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+mkdir dist
+mkdir dist/freefont
+wget https://ftp.gnu.org/gnu/freefont/freefont-ttf.zip -P dist/
+unzip dist/freefont-ttf.zip -d dist/freefont
+mkdir dist/DSEG
+wget https://github.com/keshikan/DSEG/releases/download/v0.50beta1/fonts-DSEG_v050b1.zip -P dist/
+unzip dist/fonts-DSEG_v050b1.zip -d dist/DSEG
+wget https://github.com/google/fonts/raw/main/ofl/audiowide/Audiowide-Regular.ttf -P dist/
