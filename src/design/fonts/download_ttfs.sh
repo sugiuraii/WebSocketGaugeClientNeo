768639dcfaeec7e2c5731aea2f0b185c11fa74b3
sz2 <sugiura.k.nigo@gmail.com>
2024-02-12 00:39:30 +0900

    Fix typo of download_ttfs.sh

--- src/design/fonts/download_ttfs.sh
+++ src/design/fonts/download_ttfs.sh
@@ -8,4 +8,4 @@ mkdir dist/DSEG
 wget https://github.com/keshikan/DSEG/releases/download/v0.50beta1/fonts-DSEG_v050b1.zip -P dist/
 unzip dist/fonts-DSEG_v050b1.zip -d dist/DSEG
 wget https://github.com/google/fonts/raw/main/ofl/audiowide/Audiowide-Regular.ttf -P dist/
-wget hhttps://fonts.cdnfonts.com/s/15358/Michroma.woff -P dist/
+wget https://fonts.cdnfonts.com/s/15358/Michroma.woff -P dist/
