4ce8fe2414a4c39163c3023a98aaeae1c27bbdf6
sz2 <sugiura.k.nigo@gmail.com>
2024-02-11 18:54:19 +0900

    Change setting of AnalogSingleMeter build_fonts.sh

--- src/design/svgs/AnalogSingleMeter/build_fonts.sh
+++ src/design/svgs/AnalogSingleMeter/build_fonts.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 source ../../lib/build_fonts_functuions.sh
 
-PADDING=10
+PADDING=5
 Color=255,255,255
-Char=48-57,78,80,68,45
+Char=32-126
 
-bmfontmake Michroma-Regular.ttf Michroma_24px_Glow 24 $Color $PADDING $Char
-bmfontmake Michroma-Regular.ttf Michroma_48px_Glow 48 $Color $PADDING $Char
+bmfontmake Michroma.ttf Michroma_18px_Glow 18 $Color $PADDING $Char
+bmfontmake Michroma.ttf Michroma_36px_Glow 36 $Color $PADDING $Char
 
-BLUR_OPTION=0x7 
-glowFilter Michroma_24px_Glow_0.png $BLUR_OPTION
-glowFilter Michroma_48px_Glow_0.png $BLUR_OPTION
+BLUR_OPTION=0x2
+glowFilter Michroma_18px_Glow_0.png $BLUR_OPTION
+glowFilter Michroma_36px_Glow_0.png $BLUR_OPTION
