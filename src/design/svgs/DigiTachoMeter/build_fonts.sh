7003e13c5d224ff8e0a18f6a2df919a2703f3020
sz2 <sugiura.k.nigo@gmail.com>
2024-02-12 17:30:49 +0900

    Resize bitmapfont of DigiTachoMeter.

--- src/design/svgs/DigiTachoMeter/build_fonts.sh
+++ src/design/svgs/DigiTachoMeter/build_fonts.sh
@@ -5,9 +5,9 @@ PADDING=10
 Color=255,255,255
 Char=48-57,78,80,68,45
 
-bmfontmake Audiowide-Regular.ttf GearPosFont 125 $Color $PADDING $Char
-bmfontmake freefont/sfd/FreeSansBold.ttf SpeedMeterFont 185 $Color $PADDING $Char
+bmfontmake Audiowide-Regular.ttf GearPosFont 101 $Color $PADDING $Char
+bmfontmake freefont/sfd/FreeSansBold.ttf SpeedMeterFont 170 $Color $PADDING $Char
 
-BLUR_OPTION=0x7 
+BLUR_OPTION=0x5
 glowFilter GearPosFont_0.png $BLUR_OPTION
 glowFilter SpeedMeterFont_0.png $BLUR_OPTION
