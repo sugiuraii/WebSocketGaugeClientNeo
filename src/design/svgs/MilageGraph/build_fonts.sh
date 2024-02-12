988c118b719035a8e559ccec15de29dbb7172628
sz2 <sugiura.k.nigo@gmail.com>
2024-02-13 00:30:30 +0900

    Fix size and face of MilageGraph font build script.

--- src/design/svgs/MilageGraph/build_fonts.sh
+++ src/design/svgs/MilageGraph/build_fonts.sh
@@ -3,11 +3,13 @@ source ../../lib/build_fonts_functuions.sh
 
 PADDING=10
 Color=255,255,255
-Char=48-57,78,80,68,45
+Char=43-57
 
-bmfontmake freefont/sfd/FreeSansBold.ttf MilageGraphFont_45px 45 $Color $PADDING $Char
-bmfontmake freefont/sfd/FreeSansBold.ttf MilageGraphFont_65px 65 $Color $PADDING $Char
+bmfontmake freefont/sfd/FreeSansBold.ttf MilageGraphFont_38px 38 $Color $PADDING $Char
+bmfontmake freefont/sfd/FreeSansBold.ttf MilageGraphFont_57px 57 $Color $PADDING $Char
+replaceFace MilageGraphFont_38px.fnt MilageGraphFont_38px
+replaceFace MilageGraphFont_57px.fnt MilageGraphFont_57px
 
 BLUR_OPTION=0x7 
-glowFilter MilageGraphFont_45px_0.png $BLUR_OPTION
-glowFilter MilageGraphFont_65px_0.png $BLUR_OPTION
+glowFilter MilageGraphFont_38px_0.png $BLUR_OPTION
+glowFilter MilageGraphFont_57px_0.png $BLUR_OPTION
