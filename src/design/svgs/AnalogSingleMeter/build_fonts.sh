91b14812049eb4d1b2bc7e2b482f4d9d15c0c468
sz2 <sugiura.k.nigo@gmail.com>
2024-02-12 22:35:01 +0900

    Implement replaceFace to build_font script of AnalogSingleMeter.

--- src/design/svgs/AnalogSingleMeter/build_fonts.sh
+++ src/design/svgs/AnalogSingleMeter/build_fonts.sh
@@ -5,9 +5,11 @@ PADDING=5
 Color=255,255,255
 Char=32-126
 
-bmfontmake Michroma.ttf Michroma_18px_Glow 18 $Color $PADDING $Char
-bmfontmake Michroma.ttf Michroma_36px_Glow 36 $Color $PADDING $Char
+bmfontmake Michroma.ttf AnalogSingleMeter_18px 18 $Color $PADDING $Char
+bmfontmake Michroma.ttf AnalogSingleMeter_36px 36 $Color $PADDING $Char
+replaceFace AnalogSingleMeter_18px.fnt AnalogSingleMeter_18px
+replaceFace AnalogSingleMeter_36px.fnt AnalogSingleMeter_36px
 
 BLUR_OPTION=0x2
-glowFilter Michroma_18px_Glow_0.png $BLUR_OPTION
-glowFilter Michroma_36px_Glow_0.png $BLUR_OPTION
+glowFilter AnalogSingleMeter_18px_0.png $BLUR_OPTION
+glowFilter AnalogSingleMeter_36px_0.png $BLUR_OPTION
