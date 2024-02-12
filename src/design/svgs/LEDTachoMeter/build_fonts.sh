49f2cc6e720d2bbbfb1446ad1a852a748f7790e2
sz2 <sugiura.k.nigo@gmail.com>
2024-02-13 00:50:49 +0900

    Fix typo of build_fonts at LEDTachoMeter.

--- src/design/svgs/LEDTachoMeter/build_fonts.sh
+++ src/design/svgs/LEDTachoMeter/build_fonts.sh
@@ -3,13 +3,13 @@ source ../../lib/build_fonts_functuions.sh
 
 PADDING=10
 Color=255,255,255
-Char=48-57,78,80,68,45
+Char=43-57,78,80,68,96,39,126
 
 bmfontmake DSEG/DSEG14-Classic/DSEG14Classic-BoldItalic.ttf LEDMeterFont_30px 30 $Color $PADDING $Char
 bmfontmake DSEG/DSEG14-Classic/DSEG14Classic-BoldItalic.ttf LEDMeterFont_45px 45 $Color $PADDING $Char
 bmfontmake DSEG/DSEG14-Classic/DSEG14Classic-BoldItalic.ttf LEDMeterFont_88px 88 $Color $PADDING $Char
 bmfontmake DSEG/DSEG14-Classic/DSEG14Classic-BoldItalic.ttf LEDMeterFont_100px 100 $Color $PADDING $Char
-replaceFace LEDMeterFont_30px.fnt CLEDMeterFont_30px
+replaceFace LEDMeterFont_30px.fnt LEDMeterFont_30px
 replaceFace LEDMeterFont_45px.fnt LEDMeterFont_45px
 replaceFace LEDMeterFont_88px.fnt LEDMeterFont_88px
 replaceFace LEDMeterFont_100px.fnt LEDMeterFont_100px
