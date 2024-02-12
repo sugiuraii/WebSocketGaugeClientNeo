e095f3ce9e82aaa6162dd5584a6d8783680510f5
sz2 <sugiura.k.nigo@gmail.com>
2024-02-12 22:14:39 +0900

    Implement replaceFace to DigiTachoMeter build_font script

--- src/design/svgs/DigiTachoMeter/build_fonts.sh
+++ src/design/svgs/DigiTachoMeter/build_fonts.sh
@@ -8,6 +8,9 @@ Char=48-57,78,80,68,45
 bmfontmake Audiowide-Regular.ttf GearPosFont 101 $Color $PADDING $Char
 bmfontmake freefont/sfd/FreeSansBold.ttf SpeedMeterFont 170 $Color $PADDING $Char
 
+replaceFace GearPosFont.fnt DigiTacho_GearPos
+replaceFace SpeedMeterFont.fnt DigiTacho_SpeedMeter
+
 BLUR_OPTION=0x5
 glowFilter GearPosFont_0.png $BLUR_OPTION
 glowFilter SpeedMeterFont_0.png $BLUR_OPTION
