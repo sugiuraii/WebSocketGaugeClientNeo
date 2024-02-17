f81d10398c2fd04ed65e5949e1bc04cc995c3a4a
sz2 <sugiura.k.nigo@gmail.com>
2024-02-18 00:48:28 +0900

    Add goldman-regular to download_ttfs.sh.

--- src/design/fonts/download_ttfs.sh
+++ src/design/fonts/download_ttfs.sh
@@ -9,3 +9,4 @@ wget https://github.com/keshikan/DSEG/releases/download/v0.50beta1/fonts-DSEG_v0
 unzip dist/fonts-DSEG_v050b1.zip -d dist/DSEG
 wget https://github.com/google/fonts/raw/main/ofl/audiowide/Audiowide-Regular.ttf -P dist/
 wget https://fonts.cdnfonts.com/s/15358/Michroma.ttf -P dist/
+wget https://github.com/google/fonts/raw/main/ofl/goldman/Goldman-Regular.ttf -P dist/
